5e29a6341f93dbe689694f8e4c06c0db3813e8c6
Kenroy George <kenroy@garakami.com>
2021-02-09 00:27:26 +0100

    add Dockerfile and cloud build process

--- src/common/pgHelper.js
+++ src/common/pgHelper.js
@@ -2,19 +2,20 @@ const { Pool, Client } = require("pg");
 
 const config = require("../config/config");
 
+const dbSocketPath = process.env.DB_SOCKET_PATH || "/cloudsql";
+
 const pool = new Pool({
-	user: config.relational_db.user,
-	host: config.relational_db.host,
-	database: config.relational_db.name,
-	password: config.relational_db.pass,
-	port: config.relational_db.port,
-	max: 10,
-	idleTimeoutMillis: 10000,
-	connectionTimeoutMillis: 11000
+   user: config.relational_db.user,
+   host: `${dbSocketPath}/luhu-development:us-east1:luhu-development-postgres`,
+   database: config.relational_db.name,
+   password: config.relational_db.pass,
+   max: 10,
+   idleTimeoutMillis: 10000,
+   connectionTimeoutMillis: 11000,
 });
 
 module.exports = {
-	getClientFromPool: () => {
-		return pool.connect();
-	}
+   getClientFromPool: () => {
+      return pool.connect();
+   },
 };
--- src/config/dev.js
+++ src/config/dev.js
@@ -1,25 +1,27 @@
 module.exports = {
-    relational_db: {
-        host: "localhost",
-        port: 5432,
-        user: "postgres",
-        pass: "sqlPwd12#",
-        name: "Luhu"
-    },
-    app: {
-        port: 8081
-    },
-    firebaseProjectId: "luhu-dev.appspot.com",
-    serviceAccountKey: {
-        "type": "service_account",
-        "project_id": "luhu-dev",
-        "private_key_id": "7c94c94ca2a1a299d7c3feb8d08416fd98ef3238",
-        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCuDFVIfhW5Fp6l\n7Dl7XTN1oNGgPfFaO0V3W2y+RQqdvv4u8laD44hTdWSfXJaEOaeaVvNv/tTPLiVZ\n7NuiPdF1/ExvmwTsWWk1m8NaAaLpy+gVc7VP9hvJJqlxsIg7cPrtUXVs3/bPmOSx\n/dDSLpGwMRB1w3mXpuGfpwybxjJVzcD7PJe0xRWd2UeeBU7OVU0FBahb0TTLv+JC\nVbpoLFiYIqDGrRWjI2zSXdws9jZA2pj5aiSfzAIWDCVnMDdnM5Kpnyy6Hb1RCUim\nocMHr4XDlDY2KX2a+VqhOxQcNvoeu6+1yDBr10/pj7KinKVF8oKBgrZRnXJ8MZtx\nIFrqgi8DAgMBAAECggEAAJIFHDJ8si6zINi+Vwm3C1jddwGHoRH2Nm4JRfl4K/4s\nW015kY2/5WcDMtoOt1VUJhBWqeZMOyIIXDFrFKB7A86/+dKMH7G5GODhBnXusKMD\n4V06fXGYetO5uG8bVr6H7qhIdlhTVUxkMpbpIiyJkCLQI41UHQSQNrq7FmcoqC6o\nf/12uplMsBsGcHJDLGulOcDS94kvgQ5P49R5Sa0UqdmcGVPMrmD+aJPkY5NHhcyx\nkADIqXMMCwzV0AatmqlOKVZ63MJ8oivRxXxe8g8oApDmIpOKmSKa8RmenJMAtXlE\noNFvNQb2TWqC/TlJHIzDm5L0HW5YNisTRcNxsmP8oQKBgQDb1k7P3lMk61iTTThL\nfr6TfQaarqsJaYw9Z03Bed8aVN1ob/PPnyYz5lIZEt5Ao9nLT6hMhE28K8fTnui1\n/y+uLM6lOqeQMsKNmlG8EOo/dQUh/LZdncONiBXW0rNE05QubL/GlzW8T6GCdtqb\n5dTq6bOIBaQzpZowiWLznn/xkQKBgQDKrcdxreKzB1beADzYYdDjkXj2u6r0qhGs\nGkyy9Udol7RDRgZ5Uh3sOOXsysMVTdAqM1flmlEv8n2QMWD6DHoa0ItsdcZcineJ\nJyj5VxAmuHv3lNG913z117b/mwZ7pT0aaWe82RCQGr98G4m/rASBmlGas4VkXmT2\nr4b0AlqNUwKBgQCvd/FULarDry319Q7YA4RuCL86u+w9CQgtAsWnhnUzeUV4VNOQ\nhwFhL5eb99l6Us5GX4CAAqJhbbiECzTH/7W5Gn6el6CjkkCD8gcHS6ENoYPFSwbs\n1j3+O/7ihy8b4p1GKPsxsgPsGMtbbS60fNBj6MGp8obLyoS19RMMTRjmYQKBgQC2\nT6ES0a4VCioKYb5ZiYtD2IwtrNv3Xq+s1q9dXqLQI33j8a5LHSGLZhb0a+MVW7w+\n6ZZMEwaqkX5+4NvxsJ15V1PCySIYBfnGZnJF7xm5KsNgDf6ovJhL77FNtN4dBKK1\nClFlHv/2EAlkksTSo+IdznlPJeJ8SSiaeYN0JFXdvwKBgQCSti6EEROiMJiS/kIe\n0mbStGYIct2QpzbzQ6HkRxRtdzKrr0oJa/Qhw4FconzGGm/bETEABpwNxzaSLMjv\njT+wl0dLvDc/cPVjTka0XRBzFQHjeQgM6pW/vRbru7LRfYgJrHMLVxcdAwouuvhO\nuNoy2RvNZp+3YSKldJBOMhr3Ww==\n-----END PRIVATE KEY-----\n",
-        "client_email": "firebase-adminsdk-52ar2@luhu-dev.iam.gserviceaccount.com",
-        "client_id": "113787800772663393496",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-52ar2%40luhu-dev.iam.gserviceaccount.com"
-    }
+   relational_db: {
+      host: "/cloudsql/luhu-development:us-east1:luhu-development-postgres",
+      port: 5432,
+      user: "postgres",
+      pass: "N15oxye2wOeLjCvJ",
+      name: "Luhu",
+   },
+   app: {
+      port: 8081,
+   },
+   firebaseProjectId: "luhu-dev.appspot.com",
+   serviceAccountKey: {
+      type: "service_account",
+      project_id: "luhu-dev",
+      private_key_id: "7c94c94ca2a1a299d7c3feb8d08416fd98ef3238",
+      private_key:
+         "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCuDFVIfhW5Fp6l\n7Dl7XTN1oNGgPfFaO0V3W2y+RQqdvv4u8laD44hTdWSfXJaEOaeaVvNv/tTPLiVZ\n7NuiPdF1/ExvmwTsWWk1m8NaAaLpy+gVc7VP9hvJJqlxsIg7cPrtUXVs3/bPmOSx\n/dDSLpGwMRB1w3mXpuGfpwybxjJVzcD7PJe0xRWd2UeeBU7OVU0FBahb0TTLv+JC\nVbpoLFiYIqDGrRWjI2zSXdws9jZA2pj5aiSfzAIWDCVnMDdnM5Kpnyy6Hb1RCUim\nocMHr4XDlDY2KX2a+VqhOxQcNvoeu6+1yDBr10/pj7KinKVF8oKBgrZRnXJ8MZtx\nIFrqgi8DAgMBAAECggEAAJIFHDJ8si6zINi+Vwm3C1jddwGHoRH2Nm4JRfl4K/4s\nW015kY2/5WcDMtoOt1VUJhBWqeZMOyIIXDFrFKB7A86/+dKMH7G5GODhBnXusKMD\n4V06fXGYetO5uG8bVr6H7qhIdlhTVUxkMpbpIiyJkCLQI41UHQSQNrq7FmcoqC6o\nf/12uplMsBsGcHJDLGulOcDS94kvgQ5P49R5Sa0UqdmcGVPMrmD+aJPkY5NHhcyx\nkADIqXMMCwzV0AatmqlOKVZ63MJ8oivRxXxe8g8oApDmIpOKmSKa8RmenJMAtXlE\noNFvNQb2TWqC/TlJHIzDm5L0HW5YNisTRcNxsmP8oQKBgQDb1k7P3lMk61iTTThL\nfr6TfQaarqsJaYw9Z03Bed8aVN1ob/PPnyYz5lIZEt5Ao9nLT6hMhE28K8fTnui1\n/y+uLM6lOqeQMsKNmlG8EOo/dQUh/LZdncONiBXW0rNE05QubL/GlzW8T6GCdtqb\n5dTq6bOIBaQzpZowiWLznn/xkQKBgQDKrcdxreKzB1beADzYYdDjkXj2u6r0qhGs\nGkyy9Udol7RDRgZ5Uh3sOOXsysMVTdAqM1flmlEv8n2QMWD6DHoa0ItsdcZcineJ\nJyj5VxAmuHv3lNG913z117b/mwZ7pT0aaWe82RCQGr98G4m/rASBmlGas4VkXmT2\nr4b0AlqNUwKBgQCvd/FULarDry319Q7YA4RuCL86u+w9CQgtAsWnhnUzeUV4VNOQ\nhwFhL5eb99l6Us5GX4CAAqJhbbiECzTH/7W5Gn6el6CjkkCD8gcHS6ENoYPFSwbs\n1j3+O/7ihy8b4p1GKPsxsgPsGMtbbS60fNBj6MGp8obLyoS19RMMTRjmYQKBgQC2\nT6ES0a4VCioKYb5ZiYtD2IwtrNv3Xq+s1q9dXqLQI33j8a5LHSGLZhb0a+MVW7w+\n6ZZMEwaqkX5+4NvxsJ15V1PCySIYBfnGZnJF7xm5KsNgDf6ovJhL77FNtN4dBKK1\nClFlHv/2EAlkksTSo+IdznlPJeJ8SSiaeYN0JFXdvwKBgQCSti6EEROiMJiS/kIe\n0mbStGYIct2QpzbzQ6HkRxRtdzKrr0oJa/Qhw4FconzGGm/bETEABpwNxzaSLMjv\njT+wl0dLvDc/cPVjTka0XRBzFQHjeQgM6pW/vRbru7LRfYgJrHMLVxcdAwouuvhO\nuNoy2RvNZp+3YSKldJBOMhr3Ww==\n-----END PRIVATE KEY-----\n",
+      client_email: "firebase-adminsdk-52ar2@luhu-dev.iam.gserviceaccount.com",
+      client_id: "113787800772663393496",
+      auth_uri: "https://accounts.google.com/o/oauth2/auth",
+      token_uri: "https://oauth2.googleapis.com/token",
+      auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+      client_x509_cert_url:
+         "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-52ar2%40luhu-dev.iam.gserviceaccount.com",
+   },
 };
