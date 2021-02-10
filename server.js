13d6841e75df78a34d211fd89fbb6ada5def1f8a
Kenroy George <kenroy@garakami.com>
2021-02-10 23:08:13 +0100

    update url paths for api

--- server.js
+++ server.js
@@ -4,27 +4,26 @@ const cors = require("cors");
 const config = require("./src/config/config");
 const app = express();
 
-const userRouter = require('./src/users');
-const offersRouter = require('./src/offers');
-const userInvitesRouter = require('./src/userInvites');
-const notificationsRouter = require('./src/notifications');
-const chatsRouter = require('./src/chats');
-const messagesRouter = require('./src/messages');
-const stripeRouter = require('./src/stripe');
-
+const userRouter = require("./src/users");
+const offersRouter = require("./src/offers");
+const userInvitesRouter = require("./src/userInvites");
+const notificationsRouter = require("./src/notifications");
+const chatsRouter = require("./src/chats");
+const messagesRouter = require("./src/messages");
+const stripeRouter = require("./src/stripe");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/offers', offersRouter);
-app.use('/api/v1/userInvites', userInvitesRouter);
-app.use('/api/v1/notifications', notificationsRouter);
-app.use('/api/v1/chats', chatsRouter);
-app.use('/api/v1/messages', messagesRouter);
-app.use('/api/v1/stripe', stripeRouter);
-app.listen(config.app.port, () => {
-    console.log(`Example app listening on port ${config.app.port}!`)
-});
+app.use("/users", userRouter);
+app.use("/offers", offersRouter);
+app.use("/userInvites", userInvitesRouter);
+app.use("/notifications", notificationsRouter);
+app.use("/chats", chatsRouter);
+app.use("/messages", messagesRouter);
+app.use("/stripe", stripeRouter);
 
+app.listen(config.app.port, () => {
+   console.log(`Example app listening on port ${config.app.port}!`);
+});
