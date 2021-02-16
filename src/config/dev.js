be59052523ac51134cc6078ed86ded8523190ee3
jsmitrah <ram@mitrahsoft.com>
2021-02-16 11:49:53 +0530

    small updates

--- src/config/dev.js
+++ src/config/dev.js
@@ -4,7 +4,7 @@ module.exports = {
       port: 5432,
       user: "postgres",
       pass: "n9Lyi5Eboh1O3KKt",
-      name: "luhu-development",
+      name: "Luhu",
       cloudpath: "/cloudsql/luhu-dev:us-central1:luhu-development-db-postgres",
    },
    app: {
