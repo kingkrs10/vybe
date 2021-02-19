e8b6cc4ef0700e048560480d521cadcc392cfbdd
jsmitrah <ram@mitrahsoft.com>
2021-02-19 12:30:46 +0530

    * Implemented the firebase push notification.

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
 const cors = require("cors");
 const config = require("./src/config/config");
 const app = express();
+const { firebaseAdmin } = require("./src/common/firebase");
+const { authMiddleware, upload } = require('./src/common/common');
 
 const userRouter = require("./src/users");
 const offersRouter = require("./src/offers");
@@ -11,6 +12,10 @@ const notificationsRouter = require("./src/notifications");
 const chatsRouter = require("./src/chats");
 const messagesRouter = require("./src/messages");
 const stripeRouter = require("./src/stripe");
+const notification_options = {
+   priority: "high",
+   timeToLive: 60 * 60 * 24
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,6 +28,28 @@ app.use("/notifications", notificationsRouter);
 app.use("/chats", chatsRouter);
 app.use("/messages", messagesRouter);
 app.use("/stripe", stripeRouter);
+
+app.post('/firebase/notification', upload.none(), authMiddleware, (req, res) => {
+   const { deviceId, name } = req.body;
+   var registrationToken = deviceId;
+   var message = {
+      data: { messageFrom: `${name}`, report: "true" },
+      notification: {
+         title: `${name}`,
+         body: `${name} ❤ favorited your post.`,
+         sound: "default",
+      },
+   };
+   const options = notification_options
+
+   firebaseAdmin.messaging().sendToDevice(registrationToken, message, options)
+      .then(response => {
+         res.status(200).send("Notification sent successfully")
+      })
+      .catch(error => {
+         console.log(error);
+      });
+})
 
 app.listen(config.app.port, () => {
    console.log(`Example app listening on port ${config.app.port}!`);
