fa9c80741dd23dec81b0c718f3223a96ab21d7e8
jsmitrah <ram@mitrahsoft.com>
2021-12-31 16:44:18 +0530

    Minor updates

--- src/offers/controller.js
+++ src/offers/controller.js
@@ -9,8 +9,10 @@ const create = async (request, response) => {
    try {
       const offerId = uuidv4();
       var imagePath = request.body.offerImage && request.body.offerImage !== '' ? request.body.offerImage : null;
-      var mediumImagePath = request.body.offerMediumImage && request.body.offerMediumImage !== '' ? request.body.offerMediumImage : null;
-      var thumpImagePath =  request.body.offerThumpImage  && request.body.offerThumpImage !== ''? request.body.offerThumpImage : null;
+      var mediumImagePath = request.body.offerImage && request.body.offerImage !== '' ? request.body.offerImage : null;
+      var thumpImagePath = request.body.offerImage && request.body.offerImage !== '' ? request.body.offerImage : null;
+      // var mediumImagePath = request.body.offerMediumImage && request.body.offerMediumImage !== '' ? request.body.offerMediumImage : null;
+      // var thumpImagePath =  request.body.offerThumpImage  && request.body.offerThumpImage !== ''? request.body.offerThumpImage : null;
 
       // const imagePathArr = await fileUploadingProcess(request.files, offerId);
 
