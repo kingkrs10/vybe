2a6649b03f9a9bbb0cf09b3e46ac7f5059950fae
Kenroy George <kenroy@garakami.com>
2023-03-06 01:57:36 +0100

    chore(prod): update config with node env variable

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,10 @@
-var fs = require("fs");
-var config = require("./production.js");
+// var fs = require("fs");
+var config;
 // TO DO : Make it using URL based
-if (fs.existsSync(__dirname + "/dev.js")) {
-    config = require("./dev.js");
+if (process.env.NODE_ENV === "development") {
+  config = require("./dev.js");
+} else {
+  config = require("./produdction.js");
 }
 
 module.exports = config;
